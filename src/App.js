1ce3d99b5b911ce430e1b83c4e208cd4b9275032
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-07-16 21:09:59 +0200

    create videoList to contain list of videos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Upload from "./components/Upload";
+import VideoList from "./components/VideoList";
 
 function App() {
   return (
     <main className="App">
       <Upload />
+      <VideoList />
     </main>
   );
 }
