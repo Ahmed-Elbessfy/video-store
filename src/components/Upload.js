832ac934b60712088a963b603de84bd6d6be616b
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-07-16 21:06:13 +0200

    add upload video functionality
    - move firebaseConfig file into src
    - create components folder to contain components
    - add upload functionality at upload component

---
+++ src/components/Upload.js
@@ -0,0 +1,87 @@
+import { useRef, useState } from "react";
+
+import { storage } from "../firbaseConfig";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+function Upload() {
+  /*************** STATE *******************/
+  // filename to use as an alternative to the upload file input
+  const [fileName, setFileName] = useState("");
+  // percent to show uploading progress
+  const [percent, setPercent] = useState(0);
+
+  // ref to the upload input
+  const uploadRef = useRef(null);
+
+  /*************** METHODS *******************/
+  function handleClick() {
+    // fire click event on upload file input
+    uploadRef.current.click();
+  }
+
+  // Upload function
+  function handleUpload(e) {
+    // get file data
+    let file = e.target.files[0];
+
+    // if no file added
+    if (!file) {
+      alert("Please choose a file first!");
+    }
+    // set alternative name
+    setFileName(file.name);
+
+    // update configuration
+    // storage ref at firebase
+    const storageRef = ref(storage, `/files/${file.name}`);
+    // update method
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      // during upload in progress
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+
+        // update progress
+        setPercent(percent);
+      },
+      // upload error
+      (err) => console.log(err),
+      // upload success
+      () => {
+        // download url
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((url) => {
+            console.log(url);
+          })
+          .then(() => {
+            // reset file name & percent
+            setFileName("");
+            setPercent(0);
+          });
+      }
+    );
+  }
+
+  return (
+    <section>
+      {/* upload file input  */}
+      <input
+        type="file"
+        ref={uploadRef}
+        onChange={handleUpload}
+        accept=""
+        style={{ display: "none" }}
+      />
+      {/* upload file button  */}
+      <button onClick={handleClick}>Select</button>
+      <p>{fileName}</p>
+      <p>{percent} "% done"</p>
+    </section>
+  );
+}
+
+export default Upload;
