030e239e032e793cc2adc64337cc96eb75566e2a
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-07-17 13:00:17 +0200

    fetch videos list from firestore collection

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getFirestore, onSnapshot, collection } from "../firbaseConfig";
 
 function VideoList() {
-  return <div>VideoList</div>;
+  // video source state
+  const [videosSource, setVideoSources] = useState([]);
+
+  // get videos from firebase data base
+  useEffect(() => {
+    let fetchVideos = onSnapshot(
+      collection(getFirestore(), "videos-collection"),
+      (snapShot) => {
+        setVideoSources(snapShot.docs.map((doc) => doc.data()));
+      }
+    );
+
+    return fetchVideos;
+  }, []);
+
+  return (
+    <div className="videos-container">
+      <h2>Videos</h2>
+      {videosSource &&
+        videosSource.map((video, i) => {
+          return (
+            <video
+              key={i}
+              src={video.videoUrl}
+              alt={video.name}
+              controls
+            ></video>
+          );
+        })}
+    </div>
+  );
 }
 
 export default VideoList;
